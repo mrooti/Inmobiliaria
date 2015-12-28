4fd8faa7006fb26a35e64d86a7e39a2689165798
Rogelio Fuentes <rogelioroger281@gmail.com>
2015-12-28 09:26:53 -0600

    Agregado de comentario

--- control/connection.php
+++ control/connection.php
@@ -1,5 +1,6 @@
 <?php
 	global $mysqli;
+	//sustituye los valores de usuario, contraseña y base de datos a los valores que tengas tú
 	$mysqli=new mysqli("localhost","usuario","contrasena","basededatos");
 	if($mysqli->errno){
 		echo "<h1>Fallo la conexión con la base de datos";
