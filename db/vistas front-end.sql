90724d5a429fa541c0d444bd2c0c4213da3f3eec
edgarhrz <edgarinhrz@gmail.com>
2016-04-01 13:01:47 -0600

    Update vistas front-end.sql

--- db/vistas front-end.sql
+++ db/vistas front-end.sql
@@ -1,1 +1,8 @@
-/* view para propiedades*/
+/* view para propiedades_precios_tipo*/
+create view prop_tipo_precio as select p.id_Propiedad Titulo, Descripcion, calle, nu_exterior, nu_interior,colonia,CP,sector,numero_control,Precio,Precio_metro, operacion, tp.Propiedad from propiedad p inner join propiedad_operacion po on p.id_Propiedad = po.Id_propiedad inner join tipo_propiedad tp on tp.idTipo_propiedad = p.Id_tipo_propiedad;
+
+/*view para propiedades y fotografias*/
+create view prop_foto as select p.id_Propiedad, Ruta from propiedad p inner join fotografia f on p.id_Propiedad = f.id_Propiedad;
+
+/*view para propiedades y atributos*/
+create view prop_atrib as select p.id_Propiedad, valor from propiedad p inner join atributo_propiedad ap on p.id_Propiedad = ap.id_propiedad;
