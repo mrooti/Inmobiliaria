9a635b1b39e63957241036ad32090cb0ef76722a
edgarhrz <edgarinhrz@gmail.com>
2016-05-02 09:47:15 -0500

    Update vistas front-end.sql

--- db/vistas front-end.sql
+++ db/vistas front-end.sql
@@ -6,3 +6,9 @@ create view prop_foto as select p.id_Propiedad, Ruta from propiedad p inner join
 
 /*view para propiedades y atributos*/
 create view prop_atrib as select p.id_Propiedad, valor, Atributo_propiedad from propiedad p inner join atributo_propiedad ap on p.id_Propiedad = ap.id_propiedad inner join atributo a on ap.id_atributo = a.id_atributo
+
+/*vista para mostrar las localidades con sus respectivos municipios y estados en una tabla*/
+create view localidades as select id_localidad,Localidad, Municipio, Estado from localidad l inner join municipio m on l.Id_municipio=m.Id_municipio inner join estado e on m.Id_estado=e.Id_estado
+
+/*vista del indexi hace referencia a la vista de las localidades*/
+create view principal as select p.id_Propiedad,operacion as movimiento,l.estado,l.municipio,l.localidad  from propiedad p inner join propiedad_operacion po on p.Id_propiedad=po.id_Propiedad inner join localidades l on p.Id_Localidad= l.id_localidad
