a559946612d6496c664af09037d2c19a4bdf28b0
Rogelio Fuentes <rogelioroger281@gmail.com>
2016-05-06 01:32:47 -0500

    Front-End

---
+++ front/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <?php
+        include("php/head.php");
+        ?>
+        <link href="css/index.css"  rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <?php
+        include("php/menu.php");
+        include("php/cuerpo.php");
+        include("php/nosotros.php"); 
+        include("php/preguntas-frecuentes.php");
+        include("php/foot.php");
+        ?>
+    </body>
+    <script src="js/jquery.backstretch.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $(".scroll").click(function(event){
+                event.preventDefault();
+                $('html,body').animate({
+                    scrollTop:$(this.hash).offset().top
+                -50},800);
+            });
+        });
+        jQuery("#slide").backstretch([
+          "img/slide1.jpg"
+          , "img/slide2.jpg"
+          , "img/slide3.jpg"
+        ], {duration: 4000, fade: 1000});
+    </script>
+</html>
---
+++ front/php/preguntas-frecuentes.php
@@ -0,0 +1,26 @@
+<div class="container-fluid cuerpo" id="preguntas-frecuentes">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-3 titulos"> 
+			<h1>Preguntas frecuentes</h1>
+		</div>
+
+		<div class="col-md-10 col-md-offset-1 descripcion"> 
+		 	<div class="panel panel-default">
+			  	<div class="panel-heading">
+			    	<h3 class="panel-title">¿Quien soy?</h3>
+			  	</div>
+			 	<div class="panel-body">
+			    	Loremipsum Loremipsum Loremipsum Loremipsum Loremipsum Loremipsum Loremipsum Loremipsum Loremipsum Loremipsum Loremipsum 
+			  	</div>
+			</div>
+			<div class="panel panel-default">
+			  	<div class="panel-heading">
+			    	<h3 class="panel-title">¿Quien soy?</h3>
+			  	</div>
+			 	<div class="panel-body">
+			    	Loremipsum Loremipsum Loremipsum Loremipsum Loremipsum Loremipsum Loremipsum Loremipsum Loremipsum Loremipsum Loremipsum 
+			  	</div>
+			</div>
+		</div>
+	</div>
+</div>
