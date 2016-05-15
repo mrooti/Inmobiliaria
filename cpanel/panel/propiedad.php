b0f4c32ae0355f67460d36792748cded475ca57d
martinalanis <martinalanis.dev@gmail.com>
2016-05-15 14:04:39 -0500

    Alta de propiedad lista

--- cpanel/panel/propiedad.php
+++ cpanel/panel/propiedad.php
@@ -8,7 +8,7 @@
 	?>
 <html>
 <head>
-<title>Página en Blanco | Inmobiliaria</title>
+<title>Agregar Propiedad | Inmobiliaria</title>
 <?php include("../estructura/head.php"); ?>
 </head> 
    
@@ -37,7 +37,8 @@
 				<div class="graphs">
 				<!--Aqui va a ir todo el contenido de cada seccion -->
 				<h3 class="blank1">Alta de Propiedad</h3>
-					<form class="form-horizontal">
+					<form class="form-horizontal" id="agregar_propiedad">
+						<legend><i>General</i></legend>
 						<div class="form-group">
 							<label for="titulo" class="col-sm-2 control-label">Título</label>
 							<div class="col-sm-8">
@@ -48,6 +49,7 @@
 							<label for="descripcion" class="col-sm-2 control-label">Descripción</label>
 							<div class="col-sm-8"><textarea name="descripcion" id="descripcion" cols="50" rows="4" class="form-control1"></textarea></div>
 						</div>
+						<legend><i>Ubicación</i></legend>
 						<div class="form-group">
 							<label for="calle" class="col-sm-2 control-label">Calle</label>
 							<div class="col-sm-8">
@@ -81,7 +83,7 @@
 						<div class="form-group">
 							<label class="col-sm-2 control-label">Estado</label>
 							<div class="col-sm-8">
-								<select class="form-control1" id="estado" name="estado">
+								<select class="form-control1" id="estado" >
 									<?php 
 									$res = $mysqli->query("select * from estado");
 									echo "<option value=''>Seleccione un estado</option>";
@@ -97,7 +99,7 @@
 						<div class="form-group">
 							<label class="col-sm-2 control-label">Municipio</label>
 							<div class="col-sm-8">
-								<select class="form-control1" id="municipio" name="municipio" disabled="disabled">
+								<select class="form-control1" id="municipio"  disabled="disabled">
 								</select>
 							</div>
 						</div>
@@ -114,6 +116,13 @@
 								<input type="text" class="form-control1" id="sector" name="sector" placeholder="">
 							</div>
 						</div>
+						<legend><i>Detalles de la propiedad</i></legend>
+						<div class="form-group">
+							<label for="num_control" class="col-sm-2 control-label">Numero de Control</label>
+							<div class="col-sm-8">
+								<input type="text" class="form-control1" id="num_control" name="num_control" placeholder="">
+							</div>
+						</div>
 						<div class="form-group">
 							<label class="col-sm-2 control-label">Tipo de Propiedad</label>
 							<div class="col-sm-8">
@@ -122,7 +131,7 @@
 									$res = $mysqli->query("select * from tipo_propiedad");
 									echo "<option value=''>Seleccione una opción</option>";
 									while ($aux = $res->fetch_assoc()) {
-										echo "<option value=".$aux['IdTipo_propiedad'].">".ucwords($aux['Propiedad'])."</option>";
+										echo "<option value=".$aux['idTipo_propiedad'].">".ucwords($aux['Propiedad'])."</option>";
 									}
 
 									?>
@@ -130,26 +139,61 @@
 							</div>
 						</div>
 						<div class="form-group">
-							<label for="num_control" class="col-sm-2 control-label">Numero de Control</label>
+							<label for="myfile" class="col-sm-2 control-label">Imagenes</label>
 							<div class="col-sm-8">
-								<input type="text" class="form-control1" id="num_control" name="num_control" placeholder="">
+								<input type="file" class="form-control1" id="myfile" name="myfile[]" placeholder="" multiple>
 							</div>
 						</div>
 						<div class="form-group">
-							<label for="sector" class="col-sm-2 control-label">Imagenes</label>
+							<label for="precio" class="col-sm-2 control-label">Precio</label>
 							<div class="col-sm-8">
-								<input type="file" multiple="" class="form-control1" id="sector" name="sector" placeholder="">
+								<input type="text" class="form-control1" id="precio" name="precio" placeholder="">
 							</div>
 						</div>
+						<div class="form-group">
+							<label for="precio_m2" class="col-sm-2 control-label">Precio por M²</label>
+							<div class="col-sm-8">
+								<input type="text" class="form-control1" id="precio_m2" name="precio_m2" placeholder="">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="radio" class="col-sm-2 control-label">Tipo</label>
+							<div class="col-sm-8">
+								<div class="radio-inline"><label><input type="radio" name="tipo" value="1"> Venta</label></div>
+								<div class="radio-inline"><label><input type="radio" name="tipo" value="2"> Renta</label></div>
+							</div>
+						</div>
+						<legend>Atributos</legend>
+						<?php 
+						$res = $mysqli->query("select * from atributo");
+						while ($aux = $res->fetch_assoc()) {
+							echo "<div class='form-group'>
+									<label for='atributos[".$aux['id_atributo']."][]' class='col-sm-2 control-label'>".ucwords($aux['Atributo_propiedad'])."</label>
+									<div class='col-sm-8'>
+										<input type='text' class='form-control1' name='atributos[".$aux['id_atributo']."]'>
+									</div>
+								</div>";
+						}
+
+						?>
 						<div class="row">
 							<div class="col-sm-8 col-sm-offset-2">
 							<!--La clase form_tp es para control en la funcion de jquery-->
 								<button type="submit" class="btn-success btn form_tp">Agregar</button>
-								<button type="reset" class="btn-default btn form_tp">Limpiar</button>
+								<button type="reset" id="btn_reset" class="btn-default btn form_tp">Limpiar</button>
 							</div>
 						</div>
 					</form>
 				<div class="clearfix"></div>
+				<br><!--Espaciado -->
+				<div class="alert alert-danger col-sm-9 col-sm-offset-1" id="msj_error">
+					No se pudo completar la operación.
+				</div>
+				<br><!--Espaciado -->
+				<div class="alert alert-success col-sm-9 col-sm-offset-1" id="msj_success">
+					Cambios guardados correctamente.
+				</div>
+				<br><!--Espaciado -->
 				</div>
 			</div>
 		</div>
@@ -161,6 +205,8 @@
 	
 <?php include("../estructura/scripts.php"); ?>
 <script>
+	//Ocultar los divs con los mensajes
+   	$("#msj_success, #msj_error").hide();
 	//Cargar el select con los municipios despues de seleccionar una opcion de estado
 	$('#estado').change(function(){
 		//limpiar el select para municipio y loclaidad
@@ -199,6 +245,37 @@
 			}
 		});
 
+	});
+
+	//Enviar formulario
+	$('form').submit(function(){
+		$("form").find(":submit").attr("disabled", "disabled");
+		var fd = new FormData(document.getElementById("agregar_propiedad"));
+		$.ajax({
+			url: "../ajax/ajax.php?opcion=15",
+			type: "POST",
+			data: fd,
+			processData: false,  // tell jQuery not to process the data
+  			contentType: false,   // tell jQuery not to set contentType
+  			success: function(data){
+  				$("#msj_success").slideDown(500).delay(2500).queue(function(n){
+					$(this).slideUp(500);
+				n();
+				});
+				$("form").find(":submit").attr("disabled", false);
+				$("#agregar_propiedad").each(function(){
+					this.reset();
+				});
+  			},
+  			error: function(data){
+  				$("#msj_error").slideDown(500).delay(2500).queue(function(n){
+					$(this).slideUp(500);
+				n();
+				});
+				$("form").find(":submit").attr("disabled", false);
+  			}
+		});
+		return false;
 	});
 </script>
 </body>
